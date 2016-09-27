291bdbebaee844f61bba8f1819a2d4467664fa6f
Stanciu Bogdan Mircea <mirceaciu@gmail.com>
2016-09-27 00:02:09 -0700

    added dropdown page sections

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
   $(".fancybox").fancybox();
 
+  $('.section-container.webapps').hide();
+  $('.section-container.maps').hide();
+  $('.section-container.db').hide();
 
+  $('.click-gis.webapss-show').click(function() {
+      $('.section-container.maps').hide();
+      $('.section-container.db').hide();
+      $('.section-container.webapps').slideDown();
+
+      $('html, body').animate({
+        scrollTop: $(".section-container.webapps").offset().top
+    }, 1000);
+  });
+
+  $('.click-gis.maps-show').click(function() {
+      $('.section-container.webapps').hide();
+      $('.section-container.db').hide();
+      $('.section-container.maps').slideDown();
+
+      $('html, body').animate({
+        scrollTop: $(".section-container.maps").offset().top
+    }, 1000);
+  });
+
+  $('.click-gis.db-show').click(function() {
+      $('.section-container.maps').hide();
+      $('.section-container.webapps').hide();
+      $('.section-container.db').slideDown();
+
+      $('html, body').animate({
+        scrollTop: $(".section-container.db").offset().top
+    }, 1000);
+  });
 });
